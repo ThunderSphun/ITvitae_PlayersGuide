69f7dddb9cb5af5be70ca15ea4637adbc6c79406
ThunderSphun <manderijn2000@gmail.com>
2024-10-23 16:48:10 +0200

    challenge 13

---
+++ src/C13.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class C13 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("There is an enemy");
+		System.out.print("What is its x?");
+		int xPos = input.nextInt();
+		System.out.print("@hat is its y?");
+		int yPos = input.nextInt();
+
+		if (xPos == 0 && yPos == 0) {
+			System.out.println("The enemy is here!");
+		} else if (xPos == 0) {
+			System.out.println("The enemy is to the " + ((yPos > 0) ? "north" : "south") + '!');
+		} else if (yPos == 0) {
+			System.out.println("The enemy is to the " + ((xPos > 0) ? "east" : "west") + '!');
+		} else {
+			System.out.println("The enemy is to the " + ((yPos > 0) ? "north" : "south") + ((xPos > 0) ? "east" : "west") + '!');
+		}
+	}
+}
