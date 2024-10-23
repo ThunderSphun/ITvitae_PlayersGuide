6c3c862d3f6a2be9514d0eb05944931edee5ae18
ThunderSphun <manderijn2000@gmail.com>
2024-10-23 13:48:01 +0200

    challenge 8

---
+++ src/C8.java
@@ -0,0 +1,10 @@
+public class C8 {
+	public static void main(String[] args) {
+		double base = 10;
+		double height = 15;
+
+		double area = base * height / 2;
+
+		System.out.printf("A triangle with a base of %f and a height of %f has an area of %f%n", base, height, area);
+	}
+}
