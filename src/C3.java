118a1e50c364c34951a5e044b07340e0e3402714
ThunderSphun <manderijn2000@gmail.com>
2024-10-23 13:06:03 +0200

    challenge 3

---
+++ src/C3.java
@@ -0,0 +1,9 @@
+public class C3 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("How are you?");
+        System.out.println("I am fine");
+        System.out.println("That is good to hear");
+        System.out.println("What's next?");
+    }
+}
